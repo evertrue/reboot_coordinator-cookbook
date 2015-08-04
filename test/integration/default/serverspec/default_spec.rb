4bf75e4f39a9c4ba9d919a3517bdf8bee4e49184
Eric Herot <eric.github@herot.com>
2015-08-04 18:00:36 -0400

    Remove the pending integration test

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,7 +4,7 @@ describe 'reboot_coordinator::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
 
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+  # it 'does something' do
+  #   skip 'Replace this with meaningful tests'
+  # end
 end
