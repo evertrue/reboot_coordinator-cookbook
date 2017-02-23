f7ef8446e0d99c282b90bea24a98c2e112743c45
Eric Herot <eric.github@herot.com>
2017-02-23 14:13:02 -0500

    Version bump 2.0.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'Apache 2.0'
 description      'Installs/Configures reboot_coordinator'
 long_description 'Installs/Configures reboot_coordinator'
-version          '2.0.2'
+version          '2.0.3'
 
 depends 'ohai', '>= 4.0'
 depends 'build-essential'
