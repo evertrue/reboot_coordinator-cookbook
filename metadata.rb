0fb6d8d4172108401b952cbef9af3357314d257f
Hai Zhou <islandsword@gmail.com>
2018-08-15 15:12:32 -0400

    Bump to v2.0.8

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'Apache 2.0'
 description      'Installs/Configures reboot_coordinator'
 long_description 'Installs/Configures reboot_coordinator'
-version          '2.0.7'
+version          '2.0.8'
 
 supports 'ubuntu', '>= 14.04'
 
